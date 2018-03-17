bd661a5b240e382fcfb33d33e1c971e977312fa1
Sergey Chernyak <chernyaksergey@gmail.com>
2018-03-17 19:49:04 +0300

    Added comment.

--- fresh_os/ubuntu_VB_guest.sh
+++ fresh_os/ubuntu_VB_guest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Install this to support ubuntu as guest inside VB
+### After install this utils shared folders will works in guest Ubuntu
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install virtualbox-guest-utils
